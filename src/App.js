82c5704bdc85a1e9ee4ae81dfde124bb40a31519
Bhargav <bhargav.satvara98@gmail.com>
2025-06-18 22:02:58 -0400

    Forgot password page design

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Index from './pages/index';
 import './assets/css/tailwind.css';
-
+import Login from './pages/pages/auth/login';
+import Signup from './pages/pages/auth/signup';
+import ForgotPassword from './pages/pages/auth/forgot-password';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Index />}></Route>
-     
+       <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
     </Routes>
   );
 }
