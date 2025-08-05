51a68d8f8e7c23cfc980d1cf1b8f79c46aa54e06
Bhargav <bhargav.satvara98@gmail.com>
2025-08-05 00:55:10 -0400

    update API base URL to production for admin and frontend services

--- admin_panel/src/services/api.js
+++ admin_panel/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
-//const API_BASE_URL = 'https://zyqora.onrender.com/api';
-const API_BASE_URL = 'http://localhost:4000/api';
+const API_BASE_URL = 'https://zyqora.onrender.com/api';
+//const API_BASE_URL = 'http://localhost:4000/api';
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
